daf7ec7f4cb1b993dbd99194473e76229df2985c
lilin <li000808@algonquinlive.com>
2023-05-29 22:55:01 -0400

    showed text widgets

--- app/src/main/java/algonquin/cst2335/li000808/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/li000808/MainActivity.java
@@ -3,6 +3,10 @@ package algonquin.cst2335.li000808;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    //look for sth with the id "theText", return that object
+    TextView theText = findViewById(R.id.theText); //same as getElementById in JS
+
+    Button b = findViewById(R.id.theButton);//will search XML for sth with id theButton
+
+    //anonymous class
+    b.setOnClickListener(new View.OnClickListener(){
+        //Provide the missing function:
+
+        @Override
+        public void onClick(View v) {
+
+            String words = theEdit.getText().toString(); //return what's in the EditText
+            //change what is in the textView;
+            theText.setText(words);
+        }
+    });
+
+    EditText theEdit = findViewById( androidx.constraintlayout.widget.R.id.bestChoice);
 }
